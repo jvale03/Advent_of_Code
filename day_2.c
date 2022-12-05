51d8b55af3c4b2b82dcbc37241d1f9e8664043ee
Joao Vale <carlosvalejcov@gmail.com>
2022-12-05 22:14:06 +0000

    day 2 complete

--- day_2.c
+++ day_2.c
@@ -18,15 +18,18 @@ int part_1(FILE *file)
         char *token_elf=strtok(line," ");
         char *token_me=strtok(NULL," ");
         token_elf[1] = '\0';
-        token_me[1] = '\0';  
-        if(token_elf==token_me)
+        token_me[1] = '\0';
+        if(strcmp(token_elf,"A")==0) token_elf="X";  
+        else if(strcmp(token_elf,"B")==0) token_elf="Y";  
+        else if(strcmp(token_elf,"C")==0) token_elf="Z";  
+        if(strcmp(token_elf,token_me)==0)
         {
             points+=3;
             if(strcmp(token_me,"X")==0) points+=X;
             else if(strcmp(token_me,"Y")==0) points+=Y;
             else if(strcmp(token_me,"Z")==0) points+=Z;
         }
-        else if((strcmp(token_elf,"A")==0 && strcmp(token_me,"Y")==0) || (strcmp(token_elf,"B")==0 && strcmp(token_me,"Z")==0) || (strcmp(token_elf,"C")==0 && strcmp(token_me,"X")==0))
+        else if((strcmp(token_elf,"X")==0 && strcmp(token_me,"Y")==0) || (strcmp(token_elf,"Y")==0 && strcmp(token_me,"Z")==0) || (strcmp(token_elf,"Z")==0 && strcmp(token_me,"X")==0))
         {
             points+=6;
             if(strcmp(token_me,"X")==0) points+=X;
@@ -43,8 +46,47 @@ int part_1(FILE *file)
     return points;
 }
 
+int part_2(FILE *file)
+{
+    int points=0;
+    size_t size=0;
+    ssize_t test=0;
+    char *line=NULL;
+    while((test=getline(&line,&size,file))!=-1)
+    {
+        char *token_elf=strtok(line," ");
+        char *token_me=strtok(NULL," ");
+        token_elf[1] = '\0';
+        token_me[1] = '\0';
+        if(strcmp(token_me,"Y")==0) //draw
+        {
+            points+=3;
+            if(strcmp(token_elf,"A")==0) points+=1;
+            else if(strcmp(token_elf,"B")==0) points+=2;
+            else if(strcmp(token_elf,"C")==0) points+=3;
+        }
+        else if(strcmp(token_me,"X")==0) //lose
+        {
+            if(strcmp(token_elf,"A")==0) points+=3;
+            else if(strcmp(token_elf,"B")==0) points+=1;
+            else if(strcmp(token_elf,"C")==0) points+=2;
+        }
+        else if(strcmp(token_me,"Z")==0) //win
+        {
+            points+=6;
+            if(strcmp(token_elf,"A")==0) points+=2;
+            else if(strcmp(token_elf,"B")==0) points+=3;
+            else if(strcmp(token_elf,"C")==0) points+=1;
+        }
+        printf("%s %s -> %d\n",token_elf,token_me,points);
+    }
+    return points;
+}
+
 int main()
 {
     FILE *file=fopen("input.txt","r");
     printf("O total de pontos foi %d.\n",part_1(file));
+    rewind(file);
+    printf("O total de pontos foi %d.\n",part_2(file));
 }
